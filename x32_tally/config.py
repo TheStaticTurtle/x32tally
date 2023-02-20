afcd5409021d1e01db41f9eb02c3e667015044d3
Samuel <samuel@thestaticturtle.fr>
2023-02-20 02:39:45 +0100

    Fixed the IP

--- x32_tally/config.py
+++ x32_tally/config.py
@@ -6,7 +6,7 @@ import board
 # this file is imported in every module
 
 # Address of the X32/M32 console
-x32_address = "192.168.150.201"
+x32_address = "192.168.150.100"
 
 # Address of the MQTT server. if mosquito is installed locally use 127.0.0.1:1883
 mqtt = {
@@ -37,21 +37,21 @@ tally_neopixel_pin = board.D18
 #   tally_leds: List of the leds for this channel
 #   on_stand_button: Pin of the stand buttons
 input_channels = {
-    1:  {"enabled": True, "tally_leds": [0,  1,   2,  3,  4,  5], "on_stand_button": board.D23},
-    2:  {"enabled": True, "tally_leds": [6,  7,   8,  9, 10, 11], "on_stand_button": board.D24},
-    3:  {"enabled": True, "tally_leds": [12, 13, 14, 15, 16, 17], "on_stand_button": board.D25},
-    4:  {"enabled": True, "tally_leds": [18, 19, 20, 21, 22, 23], "on_stand_button": board.D8},
-    5:  {"enabled": True, "tally_leds": [24, 25, 26, 27, 28, 29], "on_stand_button": board.D7},
-    6:  {"enabled": True, "tally_leds": [30, 31, 32, 33, 34, 35], "on_stand_button": board.D1},
-    7:  {"enabled": True, "tally_leds": [36, 37, 38, 39, 40, 41], "on_stand_button": board.D12},
-    8:  {"enabled": True, "tally_leds": [42, 43, 44, 45, 46, 47], "on_stand_button": board.D16},
-    9:  {"enabled": True, "tally_leds": [48, 49, 50, 51, 52, 53], "on_stand_button": board.D20},
-    10: {"enabled": True, "tally_leds": [54, 55, 56, 57, 58, 59], "on_stand_button": board.D21},
-    11: {"enabled": True, "tally_leds": [60, 61, 62, 63, 64, 65], "on_stand_button": board.D26},
-    12: {"enabled": True, "tally_leds": [66, 67, 68, 69, 70, 71], "on_stand_button": board.D19},
-    13: {"enabled": True, "tally_leds": [72, 73, 74, 75, 76, 77], "on_stand_button": board.D13},
-    14: {"enabled": True, "tally_leds": [78, 79, 80, 81, 82, 83], "on_stand_button": board.D6},
-    15: {"enabled": True, "tally_leds": [84, 85, 86, 87, 88, 89], "on_stand_button": board.D5},
+    1:  {"enabled": True,  "tally_leds": [0,  1,   2,  3,  4,  5], "on_stand_button": board.D23},
+    2:  {"enabled": True,  "tally_leds": [6,  7,   8,  9, 10, 11], "on_stand_button": board.D24},
+    3:  {"enabled": True,  "tally_leds": [12, 13, 14, 15, 16, 17], "on_stand_button": board.D25},
+    4:  {"enabled": True,  "tally_leds": [18, 19, 20, 21, 22, 23], "on_stand_button": board.D8},
+    5:  {"enabled": True,  "tally_leds": [24, 25, 26, 27, 28, 29], "on_stand_button": board.D7},
+    6:  {"enabled": True,  "tally_leds": [30, 31, 32, 33, 34, 35], "on_stand_button": board.D1},
+    7:  {"enabled": True,  "tally_leds": [36, 37, 38, 39, 40, 41], "on_stand_button": board.D12},
+    8:  {"enabled": True,  "tally_leds": [42, 43, 44, 45, 46, 47], "on_stand_button": board.D16},
+    9:  {"enabled": True,  "tally_leds": [48, 49, 50, 51, 52, 53], "on_stand_button": board.D20},
+    10: {"enabled": True,  "tally_leds": [54, 55, 56, 57, 58, 59], "on_stand_button": board.D21},
+    11: {"enabled": True,  "tally_leds": [60, 61, 62, 63, 64, 65], "on_stand_button": board.D26},
+    12: {"enabled": True,  "tally_leds": [66, 67, 68, 69, 70, 71], "on_stand_button": board.D19},
+    13: {"enabled": True,  "tally_leds": [72, 73, 74, 75, 76, 77], "on_stand_button": board.D13},
+    14: {"enabled": True,  "tally_leds": [78, 79, 80, 81, 82, 83], "on_stand_button": board.D6},
+    15: {"enabled": True,  "tally_leds": [84, 85, 86, 87, 88, 89], "on_stand_button": board.D5},
     16: {"enabled": False, "tally_leds": [90, 91, 92, 93, 94, 95], "on_stand_button": board.D0},
     17: {"enabled": False},
     18: {"enabled": False},
